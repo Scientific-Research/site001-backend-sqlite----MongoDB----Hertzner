42d54acd3202e9d0b45f610ae72facabe6c1206d
Maximilian <scientificresearch309@gmail.com>
2023-01-24 19:48:45 +0100

    _id: string;   languageText: string; added in interfaces.ts

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,11 @@
 export interface INewFlashcard {
-	category: string;
-	front: string;
-	back: string;
+  category: string;
+  front: string;
+  back: string;
 }
 
 export interface IFlashcard extends INewFlashcard {
-	id: number;
+  id: number;
+  _id: string;
+  languageText: string;
 }
